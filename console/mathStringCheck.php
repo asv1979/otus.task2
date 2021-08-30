933b3b722ce5bfa1cff045219d24dfd3024d6b78
Alexey Vasilyev <asv2108@gmail.com>
2021-08-30 16:46:05 +0300

    refactor replace autoload

--- console/mathStringCheck.php
+++ console/mathStringCheck.php
@@ -1,14 +1,12 @@
 <?php
 declare(strict_types=1);
 
-require '../vendor/autoload.php';
-
 use asv2108\String\MathStringChecker;
 
 $checker = new MathStringChecker();
 
 try {
-    $handle = fopen("../math.txt", "r");
+    $handle = fopen("math.txt", "r");
     while (!feof($handle)) {
         $buffer = fgets($handle, 4096);
         var_dump ($checker($buffer));
